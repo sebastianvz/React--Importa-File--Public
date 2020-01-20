840dc0d214219059df59a551ee3fd89bedaa1972
Sebastián Velez <devsebastianvz@gmail.com>
2020-01-20 12:56:30 -0500

    Impor data from public folder on json file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useState } from 'react';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [test2, setTest2] = useState('S'); 
+
+
+    const handleClickTest = () => {
+        debugger;
+        axios.get('./helper/varibales.json') // JSON File Path
+        .then( response => {
+            debugger;
+            let test =response.data.variable 
+            setTest2(test);
+            console.log(test);
+            console.log(test2);
+            //response.data.variable
+            
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+    const showtest=()=>
+    {
+        console.log(test2);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +38,22 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+            <p>You clicked {count} times</p>
+                <button onClick={() =>{
+                    showtest();
+                    setCount(count + 1)
+
+                } }>
+                Click me
+                </button>
+        </div>
+        <div>
+            <p>Que pasa con: {test2} de verdad.</p>
+                <button onClick={handleClickTest}>
+                Click 2
+                </button>
+        </div>
       </header>
     </div>
   );
